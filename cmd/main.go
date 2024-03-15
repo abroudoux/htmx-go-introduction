9901d5ba47e48e606583872aab377e0a6d3dd3bb
arthur <arthur.broudoux@gmail.com>
2024-03-15 11:50:00 +0100

    Count relaod page

---
+++ cmd/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"text/template"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Templates struct {
+	templates *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func NewTemplate() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("../views/*.html")),
+	}
+}
+
+type Count struct {
+	Count int
+}
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+	count := Count{Count: 0}
+	e.Renderer = NewTemplate()
+	e.GET("/", func(c echo.Context) error {
+		count.Count++
+		return c.Render(http.StatusOK, "index", count)
+	})
+
+	e.Logger.Fatal(e.Start(":42069"))
+}
