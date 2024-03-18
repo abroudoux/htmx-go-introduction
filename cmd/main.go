1437ac65b169a68430b263f3555247c335d6d79a
arthur <arthur.broudoux@gmail.com>
2024-03-18 16:09:24 +0100

    add new contact

--- cmd/main.go
+++ cmd/main.go
@@ -66,7 +66,7 @@ func main() {
 
 		data.Contacts = append(data.Contacts, newContact(name, email))
 
-		return c.Render(200, "index", data)
+		return c.Render(200, "display-contacts", data)
 	})
 
 	e.Logger.Fatal(e.Start(":42069"))
